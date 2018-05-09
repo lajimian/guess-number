1b4f384f6b5725a6289aa38245bbac966ad071d2
lajimian <472905899@qq.com>
2018-05-09 14:20:13 +0800

    完成猜数字

--- main/main.js
+++ main/main.js
@@ -1,6 +1,18 @@
 // Write your cade below:
-function main() {};
+function main(inputs) {
+    var xstr = '1234';
+    var result = ['0','A','0','B'];
+    for(var i=0;i<xstr.length;i++){   
+        var index = inputs.indexOf(xstr[i]);  
+        if(index===i)
+            result[0]=(++result[0]).toString();
+        else if(index==-1);
+        else 
+            result[2]=(++result[2]).toString();
+    }
+    result=result.join("");
 
-module.exports = {
-    main
+    return result;
 }
+
+module.exports = main;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,17 @@
 const main = require('../main/main');
 
 describe('main()', () => {
+    it('should generate string when 用户猜对', function () {
+        let inputs = '1234';
+        let result = main(inputs);
+        let expected = '4A0B';
+        expect(result).toEqual(expected);
+    });
+    it('should generate string when 用户猜错', function () {
+        let inputs = '4321';
+        let result = main(inputs);
+        let expected = '0A4B';
+        expect(result).toEqual(expected);
+    });
 
 });
